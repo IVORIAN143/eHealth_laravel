b009d6c328ffc41f41ad6f0ad2481331c4a4b72d
Glenn <glennson143@gmail.com>
2024-01-20 18:16:13 +0800

    update

--- resources/views/component/usertableaction.blade.php
+++ resources/views/component/usertableaction.blade.php
@@ -1,5 +1,5 @@
 <a class="btn-sm app-btn-primary"
-    onclick="ShowModal ('{{ $id }}', '{{ $username }}','{{ $name }}', '{{ $role }}', '{{ $email }},' )">
+    onclick="ShowModal ('{{ $id }}', '{{ $username }}', '{{ $name }}', '{{ $role }}', '{{ $email }}' )">
     edit</a>
 {{-- @if (Auth::user()->role == 'nurse')
     <a class="btn-sm app-btn-danger" data-confirm-delete="true" href="{{ route('deleteUser', ['id' => $id]) }}">Delete</a>
